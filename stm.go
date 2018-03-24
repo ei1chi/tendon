383887eea692d8f00578f3c153d0e1ce1ad3b26f
ei1chi <test@example.com>
2018-03-24 11:11:24 +0900

    fix

--- stm.go
+++ stm.go
@@ -4,19 +4,19 @@ type Stm struct {
 	state, count int
 }
 
-func (s Stm) Update() {
-	count += 1
+func (s *Stm) Update() {
+	s.count += 1
 }
 
-func (s Stm) Elapsed() int {
+func (s *Stm) Elapsed() int {
 	return s.count
 }
 
-func (s Stm) Transition(next int) {
+func (s *Stm) Transition(next int) {
 	s.state = next
 	s.count = 0
 }
 
-func (s Stm) Continue(next int) {
+func (s *Stm) Continue(next int) {
 	s.state = next
 }
