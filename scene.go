b3bea73b550cf0d223e2a1ed641231899f653e5f
ei1chi <test@example.com>
2018-03-29 08:12:48 +0900

    change interface

--- scene.go
+++ scene.go
@@ -9,44 +9,57 @@ import (
 type Scene interface {
 	Load()
 	Update(*et.Image) (Scene, error)
+	ParentScene() Scene
+}
+
+func RootScene(s Scene) Scene {
+	for s.ParentScene() != nil {
+		s = s.ParentScene()
+	}
+	return s
 }
 
 type SceneBase struct {
-	Slave     Scene
-	task      chan struct{}
-	hasLoaded bool
+	next, Parent Scene
+	task         chan struct{}
+	hasLoaded    bool
 }
 
-func (s *SceneBase) StartSlaveLoading() error {
+func (s *SceneBase) ParentScene() Scene {
+	return s.Parent
+}
+
+func (s *SceneBase) StartNextLoading(next Scene) error {
 	if s.task != nil {
-		return errors.New("slave loading has already started!")
+		return errors.New("next loading has already started!")
 	}
-	if s.Slave == nil {
-		return errors.New("slave has not created!")
+	if s.next == nil {
+		return errors.New("next has not created!")
 	}
 	s.task = make(chan struct{}, 1)
+	s.next = next
 	go func() {
-		s.Slave.Load()
+		s.next.Load()
 		<-s.task
 	}()
 	return nil
 }
 
-func (s *SceneBase) HasSlaveLoaded() (bool, error) {
+func (s *SceneBase) NextScene() (Scene, error) {
 	if s.task == nil {
-		return false, errors.New("hasn't started loading")
+		return nil, errors.New("hasn't started loading")
 	}
-	if s.Slave == nil {
-		return false, errors.New("slave has not created!")
+	if s.next == nil {
+		return nil, errors.New("next has not created!")
 	}
 	if s.hasLoaded {
-		return true, nil
+		return s.next, nil
 	}
 	select {
 	case s.task <- struct{}{}: // 空である
 		s.hasLoaded = true
-		return true, nil
+		return s.next, nil
 	default:
 	}
-	return false, nil
+	return nil, nil
 }
