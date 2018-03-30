9ccd1be3959bd4005a936728c33f3b4a09922675
ei1chi <test@example.com>
2018-03-30 13:25:35 +0900

    add function

--- bezier.go
+++ bezier.go
@@ -5,3 +5,12 @@ func Bezier3d(a, b, c, d complex128, time float64) complex128 {
 	T := complex(1-time, 0)
 	return T*T*T*a + 3*T*T*t*b + 3*T*t*t*c + t*t*t*d
 }
+
+func TweenRatio(begin, end, time float64) float64 {
+	a := complex(0, 0)
+	b := complex(begin, 0)
+	c := complex(end, 1)
+	d := 1 + 1i
+	r := Bezier3d(a, b, c, d, time)
+	return imag(r)
+}
